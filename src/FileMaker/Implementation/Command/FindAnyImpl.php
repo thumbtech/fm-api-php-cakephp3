9b41abaad25f40e69658d309552b4b46ae685876
thumbtech <thumbtech@gmail.com>
2015-05-02 12:00:57 -0400

    Updated FileMaker files

---
+++ src/FileMaker/Implementation/Command/FindAnyImpl.php
@@ -0,0 +1,25 @@
+<?php
+// updated for PHP 5.4 / CakePHP 3.0
+namespace Filemaker\Implementation\Command;
+
+use FileMaker\FileMaker;
+use Filemaker\Implementation\Command\FileMaker_Command_Find_Implementation;
+
+  require_once dirname(__FILE__) . '/FindImpl.php';
+ class FileMaker_Command_FindAny_Implementation extends FileMaker_Command_Find_Implementation
+{
+  function __construct($V0ab34ca9, $Vc6140495) {
+ FileMaker_Command_Find_Implementation::FileMaker_Command_Find_Implementation($V0ab34ca9, $Vc6140495);
+}
+
+  function &execute()
+ { 
+ $V21ffce5b = $this->_getCommandParams(); 
+ $V21ffce5b['-findany'] = true; 
+ $V0f635d0e = $this->_fm->_execute($V21ffce5b);
+if (FileMaker::isError($V0f635d0e)) {
+ return $V0f635d0e;
+} 
+ return $this->_getResult($V0f635d0e);
+}
+}
---
+++ src/FileMaker/Implementation/ResultImpl.php
@@ -0,0 +1,55 @@
+<?php
+// updated for PHP 5.4 / CakePHP 3.0
+namespace FileMaker\Implementation;
+
+  class FileMaker_Result_Implementation
+{
+  var $_fm;
+ var $_layout;
+ var $_records;
+ var $_tableCount;
+ var $_foundSetCount;
+ var $_fetchCount;
+ function __construct(&$V0ab34ca9)
+ {
+ $this->_fm = &$V0ab34ca9;
+}
+ function &getLayout()
+ {
+ return $this->_layout;
+}
+ function &getRecords()
+ {
+ return $this->_records;
+}
+ function getFields()
+ {
+ return $this->_layout->listFields();
+}
+ function getRelatedSets()
+ {
+ return $this->_layout->listRelatedSets();
+}
+ function getTableRecordCount()
+ {
+ return $this->_tableCount;
+}
+ function getFoundSetCount()
+ {
+ return $this->_foundSetCount;
+}
+ function getFetchCount()
+ {
+ return $this->_fetchCount;
+}
+
+  function getFirstRecord()
+ {
+ return $this->_records[0];
+}
+
+  function getLastRecord()
+ {
+ return $this->_records[sizeof($this->_records)-1];
+}
+}
